3a32de0d227c3549078d987a96083c46cdb2c8d0
melvalanderson1-maker <melvalanderson1@gmail.com>
2026-05-14 16:07:57 -0500

    COREEC BASE DE DATOS

--- src/config/db.js
+++ src/config/db.js
@@ -1,8 +1,12 @@
 const mysql = require("mysql2/promise");
-require("dotenv").config({ path: __dirname + "/../../.env" });
+const path = require("path");
+require("dotenv").config({ path: path.resolve(__dirname, "../../.env") });
+
+console.log("DB_USER:", process.env.DB_USER);
+console.log("DB_PASSWORD:", process.env.DB_PASSWORD);
 
 const pool = mysql.createPool({
-    host: process.env.DB_HOST || "127.0.0.1",
+    host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
