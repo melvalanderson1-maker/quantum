573fd7a5a7a8cc2dfb1b388d17b19cfadd3cff0e
melvalanderson1-maker <melvalanderson1@gmail.com>
2026-05-15 17:26:32 -0500

    FULL SATCK

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ const io = new Server(server, {
 
     cors: {
         origin: [
-            " https://fygradev.gruecolimp.com",
+            "https://fygradev.gruecolimp.com",
 
            
         ],
